75116584c0a8b9c995b41ec6cde00f9fcbef2cf0
Sermadl <rlatpdms1014@gmail.com>
2024-12-04 16:21:36 +0900

    refactor: 불필요한 빈 삭제

--- src/main/java/com/codetest/post/model/dto/request/PostCreateRequest.java
+++ src/main/java/com/codetest/post/model/dto/request/PostCreateRequest.java
@@ -1,6 +1,5 @@
 package com.codetest.post.model.dto.request;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +10,6 @@ import java.util.List;
 @Setter
 public class PostCreateRequest {
     private String title;
-    @NotNull(message = "내용을 입력해주세요")
     private String content;
     private List<MultipartFile> files;
 }
